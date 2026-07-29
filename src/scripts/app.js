16ed12bf13d5f8f0afa3f0bc4c205627eb13de04
mithilaacademic123-sudo <mithila.academic123@gmail.com>
2026-07-29 13:20:49 +0530

    js: define state array data structure

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,3 +3,6 @@ const todoForm = document.getElementById('todo-form');
 const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 const taskCounter = document.getElementById('task-counter');
+
+
+let todos = [];
