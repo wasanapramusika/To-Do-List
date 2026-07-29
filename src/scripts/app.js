dc7b0f98f1e1edd2744ebb897a81d06f2b984d6a
mithilaacademic123-sudo <mithila.academic123@gmail.com>
2026-07-29 13:17:47 +0530

    app.js: Select DOM Elements

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Frontend workspace pipeline initialized successfully.');
-});
+
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+const taskCounter = document.getElementById('task-counter');
